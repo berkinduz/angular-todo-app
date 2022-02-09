5981183ac8a0546453c0e3c751066257f79f8816
berkinduz <duzberkin@gmail.com>
2022-02-09 14:09:35 +0300

    todo style completed

---
+++ src/app/Todos.ts
@@ -0,0 +1,5 @@
+export class Todos {
+  task!: string
+  isDone!: boolean
+  priority: string = ''
+}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { Todos } from '../Todos'
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  newTask: string = ''
+  priority: string = 'Priority'
+  priorities = [
+    {
+      id: 1,
+      name: 'high',
+    },
+    {
+      id: 2,
+      name: 'low',
+    },
+    { id: 3, name: 'normal' },
+  ]
 
-  constructor() { }
+  todos: Todos[] = [
+    {
+      task: 'My Todo',
+      isDone: false,
+      priority: '',
+    },
+    {
+      task: 'My other Todo',
+      isDone: false,
+      priority: '',
+    },
+  ]
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addTask() {
+    this.todos.push({
+      task: this.newTask,
+      isDone: false,
+      priority: this.priority,
+    })
+
+    this.newTask = ''
+    this.priority = 'Priority'
   }
 
+  doneTask(index: number) {
+    this.todos[index].isDone = true
+  }
+
+  deleteTask(task: string) {
+    this.todos = this.todos.filter((todo) => todo.task !== task)
+  }
 }
